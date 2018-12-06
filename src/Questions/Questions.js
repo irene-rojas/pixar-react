936df4a96dc9438ff5e0582f85f2e61df98c3c32
Irene R <irene@smallwondermedia.com>
2018-12-05 20:22:35 -0500

    radio buttons experiments

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -8,24 +8,21 @@ const Questions = props => (
 
     <div className="questionsDiv">
 
-        {props.questions.length ? (
-            <div>
-                {props.questions.map(question => (
-                    <QuestionModel
-                        key={question.id}
-                        id={question.id}
-                        text={question.text}
-                    />
+        <ol>
+            {props.questions.map(question => (
+                <QuestionModel
+                    key={question.id}
+                    text={question.text}
+                />
+                
             ))}
-            </div>
-        ) : (
-            <h2 className="text-center">No questions</h2>
-        )}
+            <ol>
+                <Radio />
+            </ol>
+        </ol>
 
     </div>
 
 )
-
-
 
 export default Questions;
