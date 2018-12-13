05c38aa075428080df8b67d7128d5179b921e8d0
Irene R <irene@smallwondermedia.com>
2018-12-12 23:12:31 -0500

    moved to Questions.js

--- src/QuestionModel/QuestionModel.js
+++ src/QuestionModel/QuestionModel.js
@@ -8,10 +8,10 @@ const QuestionModel = (props) => (
 
     <div className="individualQuestion">
         <div className="text">{props.text}</div>
-        <div className="answerChoices">
-            <Answers />
-            <Radio />
-        </div>
+        {/* <div className="answerChoices"> */}
+            {/* <Answers /> */}
+            {/* <Radio /> */}
+        {/* </div> */}
     </div>
     
 )
