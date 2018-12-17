79160a63377e2a1cc4724247852db184893fd2c4
Irene R <irene@smallwondermedia.com>
2018-12-17 00:10:18 -0500

    basic elements functioning

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -183,33 +183,34 @@ class Questions extends Component {
         totalFalse: 0,
     }
 
+    componentDidMount() {
+        console.log(`TotalTrue: ${this.state.totalTrue}`);
+        console.log(`TotalFalse: ${this.state.totalFalse}`);
+    }
+
     handleChange = (selectedOption) => {
         this.setState({ selectedOption });
         console.log(`Option selected:`, selectedOption);
+        
         const answerValue = selectedOption.value;
         if (answerValue === true) {
             // console.log(answerValue);
-            this.setState({totalTrue: this.state.totalTrue + 1})
+            this.setState({totalTrue: this.state.totalTrue + 1}, () => {
+                console.log(`New TotalTrue: ${this.state.totalTrue}`);
+            });
         };
         if (answerValue === false) {
             // console.log(answerValue);
-            this.setState({totalFalse: this.state.totalFalse + 1})
+            this.setState({totalFalse: this.state.totalFalse + 1}, () => {
+                console.log(`New TotalFalse: ${this.state.totalFalse}`);
+            });
         };
-        console.log(`TotalTrue: ${this.state.totalTrue}`);
-        console.log(`TotalFalse: ${this.state.totalFalse}`);
-
-
-        // console.log(selectedOption.value);
-        // console.log(this.state.totalTrue);
-                // .find (needs to cycle through selectedOption)
-        // if (value === true)... +1
-        // if (value === false)... +1
       }
 
       // submit button
     handleFormSubmit = event => {
         event.preventDefault();
-
+        // print this.state.totalTrue, etc, here
     };
 
   
@@ -242,8 +243,20 @@ class Questions extends Component {
 
                 <button onClick={this.handleFormSubmit}>Submit</button>
 
-            </div>
 
+                <div className="masterResultsDiv">
+
+                    <div>
+                        Correct: {this.state.totalTrue}
+                    </div>
+
+                    <div>
+                        Wrong: {this.state.totalFalse}
+                    </div>
+
+                </div>
+
+            </div>
 
         )
     }
