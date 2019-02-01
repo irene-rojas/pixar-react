bad259aa74baab28f90d24470dd344d1a4ffa3aa
Irene R <irene@smallwondermedia.com>
2019-01-31 22:36:52 -0500

    gh-pages

--- build/precache-manifest.1158865f6ad452c02cb9638db7454fb2.js
+++ build/precache-manifest.1158865f6ad452c02cb9638db7454fb2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/pixar-react/static/js/runtime~main.86dcb18e.js"
   },
   {
-    "revision": "d4ad0baeaea0867b7158",
-    "url": "/pixar-react/static/js/main.d4ad0bae.chunk.js"
+    "revision": "80ba998c3ecb7a6bf853",
+    "url": "/pixar-react/static/js/main.80ba998c.chunk.js"
   },
   {
     "revision": "503fa4691ca712f7bf14",
     "url": "/pixar-react/static/js/1.503fa469.chunk.js"
   },
   {
-    "revision": "d4ad0baeaea0867b7158",
-    "url": "/pixar-react/static/css/main.12ea8754.chunk.css"
+    "revision": "80ba998c3ecb7a6bf853",
+    "url": "/pixar-react/static/css/main.24f72d7e.chunk.css"
   },
   {
-    "revision": "0a4f3a974a200fbe38050d8ca47be410",
+    "revision": "336a51e164cbe9924eaf121e7ce4a2be",
     "url": "/pixar-react/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pixar-react/precache-manifest.6c63bc130142e6a4678ad2c6191f69c9.js"
+  "/pixar-react/precache-manifest.1158865f6ad452c02cb9638db7454fb2.js"
 );
 
 workbox.clientsClaim();
