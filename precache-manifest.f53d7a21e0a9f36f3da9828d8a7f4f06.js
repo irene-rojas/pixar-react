d434e4386adc206822cf9accace6a5fbaa65aa52
Irene Rojas <irene@smallwondermedia.com>
2019-07-11 23:18:07 -0400

    Updates

---
+++ precache-manifest.f53d7a21e0a9f36f3da9828d8a7f4f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86dcb18e699e8522249b",
+    "url": "/pixar-react/static/js/runtime~main.86dcb18e.js"
+  },
+  {
+    "revision": "213df3786a1d0c5cd299",
+    "url": "/pixar-react/static/js/main.213df378.chunk.js"
+  },
+  {
+    "revision": "bf5559b3f8f44021d500",
+    "url": "/pixar-react/static/js/1.bf5559b3.chunk.js"
+  },
+  {
+    "revision": "213df3786a1d0c5cd299",
+    "url": "/pixar-react/static/css/main.066cf66b.chunk.css"
+  },
+  {
+    "revision": "48603b38bd5315a1fdcc4e1a935cf004",
+    "url": "/pixar-react/index.html"
+  }
+];
